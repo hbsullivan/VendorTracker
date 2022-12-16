5857774dcf89c8b7c27a08dfd895babbac636fdb
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-16 09:34:46 -0800

    PASS: returns list of orders

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -20,5 +20,15 @@ namespace VendorTracker.Models
       _instances.Add(this);
       Id = _instances.Count;
     }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
