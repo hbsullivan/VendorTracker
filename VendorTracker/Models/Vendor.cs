9831c06a88d18456650cb0a431f10ee0e4ace21b
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-16 09:45:24 -0800

    PASS: Adds specific order to a list in vendors constructor

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -16,6 +16,7 @@ namespace VendorTracker.Models
       Description = description;
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order>{};
     }
 
     public static void ClearAll()
@@ -31,6 +32,11 @@ namespace VendorTracker.Models
     public static Vendor Find(int searchId)
     {
       return _instances[searchId - 1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
     }
   }
 }
