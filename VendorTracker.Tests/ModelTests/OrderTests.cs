8a06f30bf86c01aee18065427b0f8273ba1a2fa6
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-16 09:13:15 -0800

    TEST: should return the title of the order

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -14,5 +14,17 @@ namespace VendorTracker.Tests
       Order newOrder = new Order("title", "description", 10, "08/30/2022");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitleOfOrder_String()
+    {
+      string title = "Bread Delivery";
+      string description = "20 loaves of bread";
+      int price = 100;
+      string date = "08/30/2022";
+      Order newOrder = new Order(title, description, price, date);
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
+    }
   }
 }
